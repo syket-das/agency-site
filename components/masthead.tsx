9748a4f1c4cc60e8c48cc712c2fd32c20fcb7c12
syket-das <saiketsd23@gmail.com>
2023-08-05 14:42:19 +0530

    sd

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -35,14 +35,8 @@ const Masthead: React.FC = () => {
 
         "
       >
-        {/* <source src="/assets/masthead-bg.m4v" type="video/mp4; codecs=hvc1" />
-        <source src="/assets/masthead-bg.webm" type="video/webm; codecs=vp9" /> */}
-
-        <source
-          src="/assets/video/landing.webm"
-          type="video/webm; codecs=vp9"
-        />
-        <source src="/assets/video/landing.mp4" type="video/mp4; codecs=vp9" />
+        <source src="/assets/masthead-bg.m4v" type="video/mp4; codecs=hvc1" />
+        <source src="/assets/masthead-bg.webm" type="video/webm; codecs=vp9" />
       </video>
       <div
         className={`flex-grow-0 pt-10 transition-opacity duration-1000 ${
@@ -59,7 +53,7 @@ const Masthead: React.FC = () => {
       <div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col">
         <h1 className="mb-6 text-4xl xl:text-5xl">TWB</h1>
         <h2 className="mb-2 text-2xl xl:text-3xl tracking-tight">
-          The Work Bazar - A Digital Agency
+          <span> The Work Bazar </span>- <span> A Digital Agency</span>
         </h2>
       </div>
       <div
