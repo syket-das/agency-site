f473b33cd546c5d10d3b603de062cee9cec9fc09
Ansh <anshpradhancareer03@gmail.com>
2023-08-23 12:34:03 +0530

    added the whatsapp chat bot

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -52,9 +52,9 @@ const Masthead: React.FC = () => {
       </div>
       <div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col">
         <h1 className="mb-6 text-5xl xl:text-6xl">🅃🅆🄱</h1>
-        <h2 className="mb-2 text-2xl xl:text-3xl tracking-tight ">
+        <h2 className="mb-2 text-2xl xl:text-5xl tracking-tight ">
           {/* <span>ʎɔuǝƃ∀ ʅɐʇᴉƃᴉᗡ ∀ - ɹɐzɐꓭ ʞɹoϺ ǝɥꓕ</span> */}
-          <span>ᴛⷮhͪeͤ Woͦrͬᴋⷦ Вⷡaͣzaͣrͬ -̄ Aͣ Dͩiͥgiͥᴛⷮaͣl Aͣgeͤncͨy</span>
+          <span>The Work Bazaar - A Digital Agency</span>
         </h2>
       </div>
       <div
